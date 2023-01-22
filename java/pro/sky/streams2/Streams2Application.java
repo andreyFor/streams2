9753967b795ff07d47b8d9288ec67ab9461e1345
Pronyushkin_qa <kollaydereva@gmail.com>
2023-01-22 11:06:34 +0300

    Initial commit

---
+++ java/pro/sky/streams2/Streams2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.streams2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Streams2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Streams2Application.class, args);
+	}
+
+}
